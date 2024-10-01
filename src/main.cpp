4deb1a24e686e48e04431a194650a19debb0ace7
Lena429 <l-frate@onu.edu>
2024-09-30 20:22:46 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,42 @@
 #include <Wire.h>
 
 #define ADDRESS 0x12        // slave address
+short mesLen;
 
 void setup(){
-    Wire.begin();
+    Wire.begin(21, 22);
 
-    Serial.begin(11520);
+    Serial.begin(115200);
 }
 
 void loop(){
+    char message[] = "Hello Kaden\0";
 
     Wire.beginTransmission(ADDRESS);
-    Wire.write("Hello");
+    // send the data
+    Wire.write(message);
     Wire.endTransmission();
+
+    Serial.println("Data Sent");
+
+    delay(500);
+
+    Wire.requestFrom(ADDRESS, strlen(message));
+
+
+    char retMes[mesLen];
+
+    int index = 0;
+    if (Wire.available()){
+        while(Wire.available()){
+        retMes[index] = Wire.read();
+        index++;
+        }
+        retMes[index] = '\0';
+
+        Serial.println("Data Received");
+        Serial.println(retMes);
+    }
 
     delay(1000);
 }
