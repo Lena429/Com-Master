1a8d4480ea15dc62972e87b062f316403e1c8e83
Lena429 <l-frate@onu.edu>
2024-10-30 19:25:04 -0400

    yep

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define ADDRESS 0x12        // slave address
+#define ADDRESS 0x41        // slave address
 short mesLen;
 
 void setup(){
     Wire.begin(21, 22);
+    Wire.setClock(400000);
 
     Serial.begin(115200);
 }
 
 void loop(){
-    char message[] = "Hello Kaden\0";
+    //char message[] = "Hello Kaden\0";
+
+    char message = 'b';
 
     Wire.beginTransmission(ADDRESS);
     // send the data
@@ -22,22 +25,22 @@ void loop(){
 
     delay(500);
 
-    Wire.requestFrom(ADDRESS, strlen(message));
+    //Wire.requestFrom(ADDRESS, 1);    //strlen(message)
 
 
-    char retMes[mesLen];
+    //char retMes[mesLen];
 
-    int index = 0;
-    if (Wire.available()){
-        while(Wire.available()){
-        retMes[index] = Wire.read();
-        index++;
-        }
-        retMes[index] = '\0';
+    // int index = 0;
+    // if (Wire.available()){
+    //     while(Wire.available()){
+    //     retMes[index] = Wire.read();
+    //     index++;
+    //     }
+    //     retMes[index] = '\0';
 
-        Serial.println("Data Received");
-        Serial.println(retMes);
-    }
+    //     Serial.println("Data Received");
+    //     Serial.println(retMes);
+    // }
 
-    delay(1000);
+    //delay(1000);
 }
